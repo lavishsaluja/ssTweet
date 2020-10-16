d67519b9e5c60a15638b0896dcd07eb6a3fdf2e0
Lavish Saluja <lavishsaluja@MacBook-Pro.local>
2020-10-17 01:33:53 +0530

    re structured the read me file to make it easier to get started with and removed the unnecessary text from readme file

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
 def main():
     url = "https://twitter.com/lavishsaluja/status/1225356209915453440"
     photo_list = save_images(url)
@@ -17,7 +16,6 @@ def main():
     auth_file_name = None
     upload_images(photo_list, album_title, auth_file_name)
     delete_images(photo_list)
-
 
 if __name__ == "__main__":
     main()
