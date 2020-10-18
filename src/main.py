c83dd98aa55f4cc37bdf12eaed182f86535b61f8
Lavish Saluja <lavishsaluja@MacBook-Pro.local>
2020-10-19 01:30:49 +0530

    perfected the screenshot process and uploading to photos -- yet to solve problem with multiple tweet fetching

--- src/main.py
+++ src/main.py
@@ -1,22 +1,23 @@
+import os
 from screenshot import save_images, delete_images
 from upload import upload_images
 import logging
 
-logging.basicConfig(
-    format='%(asctime)s %(module)s.%(funcName)s:%(levelname)s:%(message)s',
-    datefmt='%m/%d/%Y %I_%M_%S %p',
-    filename='log_file',
-    level=logging.INFO
-)
-
 def main():
-    url = "https://twitter.com/lavishsaluja/status/1314130099399061504"
-    album_title = 'super-sonic'
+    logging.basicConfig(
+        format='%(asctime)s %(module)s.%(funcName)s:%(levelname)s:%(message)s',
+        datefmt='%m/%d/%Y %I_%M_%S %p',
+        filename='log_file',
+        level=logging.INFO
+    )
+    url = "https://twitter.com/lavishsaluja/status/1290004685647052805"
+    album_title = 'spacex'
     auth_file_name = None
 
     photo_list = save_images(url)
-    # upload_images(photo_list, album_title, auth_file_name)
+    # upload_images(photo_list, album_title)
     # delete_images(photo_list)
+    
 
 if __name__ == "__main__":
     main()
